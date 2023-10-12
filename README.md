2c75c9680b692da94a10d58a251cb5c59780b6c4
Dennis Lau <dennislau@Denniss-Laptop.local>
2023-10-12 11:03:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # cresta-challenge
+
+## Prereqs
+Make sure you have all env vars set up in settings for Github runner or as .env file in folder
+
+## How to run
+1. `pip3 install -r requirements.txt`
+2. `python main.py`
+
+## Assumptions
+1. We are given an AWS_ACCESS_KEY and AWS_SECRET_KEY where we do not need to assume role to put to s3
+2. All calls that end on the targeted date are exported. I.E if a call starts on 2023-10-12 and ends on 2023-10-13, then it is counted as a phone call that occured on 2023-10-13
+3. `cresta-data-dump` bucket with correct permissions already exists on user AWS account(we should have a terraform module/cloudformation template for them to use)
+4. IP addresses for github actions are whitelisted to Clickhouse DB
+
+
+
---
+++ main.py
@@ -0,0 +1,46 @@
+import boto3
+import os
+import clickhouse_connect
+import csv
+from datetime import datetime, timedelta
+# check env vars
+try:
+    AWS_ACCESS_KEY=os.environ["AWS_ACCESS_KEY"]
+    AWS_SECRET_KEY=os.environ["AWS_SECRET_KEY"]
+    CLICKBASE_HOST=os.environ["CLICKBASE_HOST"]
+    CLICKBASE_USER=os.environ["CLICKBASE_USER"]
+    CLICKBASE_PASSWORD=os.environ["CLICKBASE_PASSWORD"]
+    CLICKBASE_PORT=os.environ["CLICKBASE_PORT"]
+except Exception as e:
+    print("Make sure to set proper env vars")
+    raise
+
+# quantile 0.9 means "value should over-predict 90% of the times"
+query="""SELECT agent_id,AVG(call_duration_sec),quantile(0.9)(call_duration_sec) 
+        FROM conversations where toDate(call_end)=toDate(yesterday()) 
+        AND call_status='Answered' 
+        GROUP BY agent_id"""
+
+# Connect to DB
+client = clickhouse_connect.get_client(host=CLICKBASE_HOST,port=8443, 
+                                 username=CLICKBASE_USER, password=CLICKBASE_PASSWORD)
+# query results
+results = client.query(query)
+# clean data into list of dict
+data = [{'uuid':result[0],'avg_time':result[1], '90th_percentile':result[2]} for result in results.result_rows]
+
+# connect to boto3
+s3 = boto3.resource('s3',aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY)
+
+bucket = "cresta-daily-dump"
+yesterday = datetime.now() - timedelta(days=1)
+filename = f"{yesterday.strftime('%Y-%m-%d')}.csv"
+
+# generate csv for boto upload
+with open(filename, 'w', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
+# upload to boto3
+s3.meta.client.upload_file(Filename = filename, Bucket= bucket, Key = filename)
